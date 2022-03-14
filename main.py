80e89aadbee59abc01c7ed5432762d50fed88ad2
Sergio <sergioandyp@gmail.com>
2022-03-14 16:02:55 -0300

    Ordenado de archivos

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#START
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from src.ui.windows.Main_Window import Ui_MainWindow
+
+app = QApplication(sys.argv)
+MainWindow = QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+sys.exit(app.exec_())
